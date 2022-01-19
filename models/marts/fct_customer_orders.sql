a1022d0a85d1e970bd42bdf8fd0b8a4d5b62aec3
tkayess@gmail.com <tkayess@gmail.com>
2022-01-19 05:56:41 +0000

    after watch and learn section

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,4 +1,5 @@
 with
+
 customers as (
 
     select * from {{ ref('stg_jaffle_shop__customers') }}
@@ -7,108 +8,52 @@ customers as (
 
 orders as (
 
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+    select * from {{ ref('int_orders') }}
 
 ),
 
-payments as (
-
-    select * from {{ ref('stg_stripe__payments') }}
-
-),
-
-customer_order_history as (
-
-    select 
-
-        customers.customer_id,
-        customers.full_name,
-        customers.surname,
-        customers.givenname,
-
-        min(order_date) as first_order_date,
-
-        min(case 
-            when orders.order_status not in ('returned','return_pending') 
-            then order_date 
-        end) as first_non_returned_order_date,
-
-        max(case 
-            when orders.order_status not in ('returned','return_pending') 
-            then order_date 
-        end) as most_recent_non_returned_order_date,
-
-        coalesce(max(user_order_seq),0) as order_count,
-
-        coalesce(count(case 
-            when orders.order_status != 'returned' 
-            then 1 end),
-            0
-        ) as non_returned_order_count,
-
-        sum(case 
-            when orders.order_status not in ('returned','return_pending') 
-            then payment_amount 
-            else 0 
-        end) as total_lifetime_value,
-
-        sum(case 
-            when orders.order_status not in ('returned','return_pending') 
-            then payment_amount 
-            else 0 
-        end)
-        / nullif(count(case 
-            when orders.order_status not in ('returned','return_pending') 
-            then 1 end),
-            0
-        ) as avg_non_returned_order_value,
-
-        array_agg(distinct orders.order_id) as order_ids
-
-    from orders
-
-    join customers
-    on orders.customer_id = customers.customer_id
-
-    left outer join payments
-    on orders.order_id = payments.order_id
-
-    where orders.order_status not in ('pending') and payments.payment_status != 'fail'
-
-    group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
-
-),
-
--- Final CTEs 
 final as (
 
-    select 
+    select
 
-        orders.order_id,
-        orders.customer_id,
+        orders.*,
+
         customers.surname,
         customers.givenname,
-        customer_order_history.first_order_date,
-        customer_order_history.order_count,
-        customer_order_history.total_lifetime_value,
-        payments.payment_amount as order_value_dollars,
-        orders.order_status,
-        payments.payment_status
+
+        -- Customer-level aggregations
+        min(orders.order_date) over(
+            partition by orders.customer_id
+        ) as first_order_date,
+
+        min(orders.valid_order_date) over(
+            partition by orders.customer_id
+        ) as first_non_returned_order_date,
+
+        max(orders.valid_order_date) over(
+            partition by orders.customer_id
+        ) as most_recent_non_returned_order_date,
+
+        count(*) over(
+            partition by orders.customer_id
+        ) as order_count,
+
+        sum(nvl2(orders.valid_order_date, 1, 0)) over(
+            partition by orders.customer_id
+        ) as non_returned_order_count,
+
+        array_agg(distinct orders.order_id) over(
+            partition by orders.customer_id
+        ) as order_ids,
+
+        sum(nvl2(orders.valid_order_date, orders.order_value_dollars, 0)) over(
+            partition by orders.customer_id
+        ) as total_lifetime_value
 
     from orders
-
-    join customers
-    on orders.customer_id = customers.customer_id
-
-    join customer_order_history
-    on orders.customer_id = customer_order_history.customer_id
-
-    left outer join payments
-    on orders.order_id = payments.order_id
-
-    where payments.payment_status != 'fail'
+    inner join customers
+        on orders.customer_id = customers.customer_id
 
 )
 
--- Simple Select Statement
 select * from final
